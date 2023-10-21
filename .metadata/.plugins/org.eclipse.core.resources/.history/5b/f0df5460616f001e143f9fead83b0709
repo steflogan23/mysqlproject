d31092cfb0ae4f81792678cce9da77308ea20541
steflogan23 <steflogan23@gmail.com>
2023-10-21 10:14:04 -0500

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5b/f0df5460616f001e143f9fead83b0709
@@ -0,0 +1,10 @@
+package projects;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ mysql-java/src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,25 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+DbConnection.getConnection();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	} //end main
+
+}
